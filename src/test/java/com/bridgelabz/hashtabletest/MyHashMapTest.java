e69cfc1722112438e1de1805cecfa317ce0544ab
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-08 10:41:41 +0530

    Add- Find frequency of words in a paragraph using linkedlist in hasTable

--- src/test/java/com/bridgelabz/hashtabletest/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/hashtabletest/MyHashMapTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.hashtabletest;
 
 import com.bridgelabz.hashtable.MyHashMap;
+import com.bridgelabz.hashtable.MyHashTable;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,5 +22,21 @@ public class MyHashMapTest {
         int frequency = myHashmap.get("to");
         Assert.assertEquals(2,frequency);
     }
-
+    @Test
+    public void giveASentence_whenWordsAreAddedToList_ShouldReturnParanoidFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep " +
+                "putting themselves into paranoid avoidable situations";
+        MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myHashTable.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            myHashTable.add(word, value);
+        }
+        int frequency = myHashTable.get("paranoid");
+        Assert.assertEquals(3,frequency);
+    }
 }
